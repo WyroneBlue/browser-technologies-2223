737ea74106fa52d60ec5239df08415cd188d5be6
WyroneBlue <wyroneblue@gmail.com>
2023-04-05 11:47:03 +0200

    static files

---
+++ public/js/thanks.js
@@ -0,0 +1,28 @@
+let counter = 5;
+const msg = document.querySelector('span');
+const body = document.querySelector('body');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    body.classList.remove('no-js');
+    msg.textContent = counter;
+});
+
+const interval = setInterval(() => {
+    counter--;
+    updateCount(counter);
+
+    if (counter === 0) {
+        redirect();
+        clearInterval(interval);
+    }
+}, 1000);
+
+function updateCount(count) {
+    msg.textContent = count;
+}
+
+function redirect() {
+    console.log('redirecting...');
+    // window.location.href = '/';
+}
